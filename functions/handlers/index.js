c0ecaa7f37730eccdfb68554d2ecd5811b2ae9c7
viveloper <ucsik1234@gmail.com>
2020-01-23 10:35:02 +0900

    added post image upload api

--- functions/handlers/index.js
+++ functions/handlers/index.js
@@ -1,7 +1,7 @@
 const firebase = require('../util/firebase');
 const { admin, db } = require('../util/firebaseAdmin');
 const { validateSignupData, validateLoginData } = require('../util/validators');
-const { storageBucket } = require('../config/firebaseConfig')
+const { storageBucket } = require('../config/firebaseConfig');
 
 exports.docs = (req, res) => {
   const data = {
@@ -9,150 +9,186 @@ exports.docs = (req, res) => {
     baseUrl: 'http://localhost:5000/viveloper-blog/us-central1/api'
   };
   res.render('index', data);
-}
+};
 
 exports.getAllSections = (req, res) => {
-  db.collection('sections').orderBy('index', 'asc').get().then(data => {
-    const sections = [];
-    data.forEach(doc => {
-      sections.push({
-        sectionId: doc.id,
-        index: doc.data().index,
-        name: doc.data().name,
-        description: doc.data().description
+  db.collection('sections')
+    .orderBy('index', 'asc')
+    .get()
+    .then(data => {
+      const sections = [];
+      data.forEach(doc => {
+        sections.push({
+          sectionId: doc.id,
+          index: doc.data().index,
+          name: doc.data().name,
+          description: doc.data().description
+        });
       });
-    });
-    return res.json({ sections });
-  }).catch(err => console.error(err));
-}
+      return res.json({ sections });
+    })
+    .catch(err => console.error(err));
+};
 
 exports.getPosts = (req, res) => {
-  db.collection('posts').where('section', '==', req.params.section).orderBy('createdAt', 'desc').get().then(data => {
-    const posts = [];
-    data.forEach(doc => {
-      posts.push({
-        postId: doc.id,
-        title: doc.data().title,
-        content: doc.data().content,
-        image: doc.data().image,
-        author: doc.data().author,
-        section: doc.data().section,
-        createdAt: doc.data().createdAt
+  db.collection('posts')
+    .where('section', '==', req.params.section)
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(data => {
+      const posts = [];
+      data.forEach(doc => {
+        posts.push({
+          postId: doc.id,
+          title: doc.data().title,
+          content: doc.data().content,
+          image: doc.data().image,
+          author: doc.data().author,
+          section: doc.data().section,
+          createdAt: doc.data().createdAt
+        });
       });
-    });
-    return res.json({ posts });
-  }).catch(err => console.error(err));
-}
+      return res.json({ posts });
+    })
+    .catch(err => console.error(err));
+};
 
 exports.addPost = (req, res) => {
   const newPost = {
     title: req.body.title,
     content: req.body.content,
-    image: req.body.image ? req.body.image : 'https://source.unsplash.com/random',
+    image: req.body.image
+      ? req.body.image
+      : 'https://source.unsplash.com/random',
     author: req.user.nickname,
     section: req.params.section,
     createdAt: new Date().toISOString()
   };
 
-  db.collection('posts').add(newPost).then(doc => {
-    res.status(201).json({ 
-      postId: doc.id,
-      message: `document ${doc.id} created successfully` 
+  db.collection('posts')
+    .add(newPost)
+    .then(doc => {
+      res.status(201).json({
+        postId: doc.id,
+        message: `document ${doc.id} created successfully`
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: err.message });
     });
-  }).catch(err => {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  });
-}
+};
 
 exports.getPost = (req, res) => {
-  db.collection('posts').doc(req.params.id).get().then(doc => {
-    return res.json({ post: doc.data() })
-  }).catch(err => console.error(err));
-}
+  db.collection('posts')
+    .doc(req.params.id)
+    .get()
+    .then(doc => {
+      return res.json({ post: doc.data() });
+    })
+    .catch(err => console.error(err));
+};
 
 exports.signup = (req, res) => {
   const newUser = {
     email: req.body.email,
     nickname: req.body.nickname,
     password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-  }
+    confirmPassword: req.body.confirmPassword
+  };
 
   // == validate data
-  const validationResult = validateSignupData(newUser)
-  if (!validationResult.valid) return res.status(400).json(validationResult.errors);
+  const validationResult = validateSignupData(newUser);
+  if (!validationResult.valid)
+    return res.status(400).json(validationResult.errors);
   // ==
 
   const noImg = 'no-img.png';
 
-  db.collection('users').doc(newUser.nickname).get().then(doc => {
-    if (doc.exists) {
-      return res.status(400).json({ nickname: 'this nickname is already taken' })
-    }
-    else {
-      let userId = null;
-      let token = null;
-      return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password).then(data => {
-        userId = data.user.uid;
-        return data.user.getIdToken();
-      }).then(idToken => {
-        token = idToken;
-        const userCredentials = {
-          nickname: newUser.nickname,
-          email: newUser.email,
-          createdAt: new Date().toISOString(),
-          imageUrl: `https://firebasestorage.googleapis.com/v0/b/${storageBucket}/o/${noImg}?alt=media`,
-          userId,
-          isAdmin: false
-        }
-        return db.collection('users').doc(newUser.nickname).set(userCredentials);
-      }).then(() => {
-        return res.status(201).json({ token });
-      }).catch(err => {
-        console.error(err);
-        if (err.code === 'auth/email-already-in-use') {
-          return res.status(400).json({ email: 'Email is already in use' })
-        }
-        else if (err.code === 'auth/weak-password') {
-          return res.status(400).json({ password: 'Password should be at least 6 characters' })
-        }
-        else {
-          return res.status(500).json({ general: err.message });
-        }
-      })
-    }
-  })
-}
+  db.collection('users')
+    .doc(newUser.nickname)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        return res
+          .status(400)
+          .json({ nickname: 'this nickname is already taken' });
+      } else {
+        let userId = null;
+        let token = null;
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password)
+          .then(data => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+          })
+          .then(idToken => {
+            token = idToken;
+            const userCredentials = {
+              nickname: newUser.nickname,
+              email: newUser.email,
+              createdAt: new Date().toISOString(),
+              imageUrl: `https://firebasestorage.googleapis.com/v0/b/${storageBucket}/o/${noImg}?alt=media`,
+              userId,
+              isAdmin: false
+            };
+            return db
+              .collection('users')
+              .doc(newUser.nickname)
+              .set(userCredentials);
+          })
+          .then(() => {
+            return res.status(201).json({ token });
+          })
+          .catch(err => {
+            console.error(err);
+            if (err.code === 'auth/email-already-in-use') {
+              return res.status(400).json({ email: 'Email is already in use' });
+            } else if (err.code === 'auth/weak-password') {
+              return res
+                .status(400)
+                .json({ password: 'Password should be at least 6 characters' });
+            } else {
+              return res.status(500).json({ general: err.message });
+            }
+          });
+      }
+    });
+};
 
 exports.login = (req, res) => {
   const user = {
     email: req.body.email,
     password: req.body.password
-  }
+  };
 
   // == validation data
-  const validationResult = validateLoginData(user)
-  if (!validationResult.valid) return res.status(400).json(validationResult.errors);
+  const validationResult = validateLoginData(user);
+  if (!validationResult.valid)
+    return res.status(400).json(validationResult.errors);
   // ==
 
-  firebase.auth().signInWithEmailAndPassword(user.email, user.password).then(data => {
-    return data.user.getIdToken();
-  }).then(token => {
-    return res.json({ token });
-  }).catch(err => {
-    console.error(err);
-    if (err.code === 'auth/wrong-password') {
-      return res.status(400).json({ password: 'wrong password' });
-    }
-    else if (err.code === 'auth/user-not-found') {
-      return res.status(400).json({ email: 'user not found' });
-    }
-    else {
-      return res.status(500).json({ general: err.message });
-    }
-  })
-}
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === 'auth/wrong-password') {
+        return res.status(400).json({ password: 'wrong password' });
+      } else if (err.code === 'auth/user-not-found') {
+        return res.status(400).json({ email: 'user not found' });
+      } else {
+        return res.status(500).json({ general: err.message });
+      }
+    });
+};
 
 exports.uploadUserImage = (req, res) => {
   const Busboy = require('busboy');
@@ -171,31 +207,92 @@ exports.uploadUserImage = (req, res) => {
     }
 
     const imageExtension = filename.split('.')[filename.split('.').length - 1];
-    imageFileName = `${Math.round(Math.random() * 100000000000)}.${imageExtension}`
+    imageFileName = `${Math.round(
+      Math.random() * 100000000000
+    )}.${imageExtension}`;
     const filepath = path.join(os.tmpdir(), imageFileName);
     imageToBeUploaded = { filepath, mimeType };
     file.pipe(fs.createWriteStream(filepath));
   });
 
   busboy.on('finish', () => {
-    admin.storage().bucket(storageBucket).upload(imageToBeUploaded.filepath, {
-      resumable: false,
-      metadata: {
+    admin
+      .storage()
+      .bucket(storageBucket)
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
         metadata: {
-          contentType: imageToBeUploaded.mimeType
+          metadata: {
+            contentType: imageToBeUploaded.mimeType
+          }
         }
-      }
-    }).then(() => {
-      const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${storageBucket}/o/${imageFileName}?alt=media`;
-      console.log(imageUrl);
-      db.collection('users').doc(req.user.nickname).update({ imageUrl });
-    }).then(() => {
-      return res.json({ message: 'image uploaded successfully' })
-    }).catch(err => {
-      console.error(err);
-      return res.status(500).json({ general: err.message });
-    })
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${storageBucket}/o/${imageFileName}?alt=media`;
+        console.log(imageUrl);
+        db.collection('users')
+          .doc(req.user.nickname)
+          .update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: 'image uploaded successfully' });
+      })
+      .catch(err => {
+        console.error(err);
+        return res.status(500).json({ general: err.message });
+      });
   });
 
   busboy.end(req.rawBody);
-}
+};
+
+exports.uploadPostTitleImage = (req, res) => {
+  const Busboy = require('busboy');
+  const path = require('path');
+  const os = require('os');
+  const fs = require('fs');
+
+  const busboy = new Busboy({ headers: req.headers });
+
+  let imageFileName;
+  let imageToBeUploaded = {};
+
+  busboy.on('file', (fieldname, file, filename, encoding, mimeType) => {
+    if (mimeType !== 'image/jpeg' && mimeType !== 'image/png') {
+      return res.status(400).json({ error: 'Wrong file type submitted' });
+    }
+
+    const imageExtension = filename.split('.')[filename.split('.').length - 1];
+    imageFileName = `${Math.round(
+      Math.random() * 100000000000
+    )}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimeType };
+    file.pipe(fs.createWriteStream(filepath));
+  });
+
+  busboy.on('finish', () => {
+    admin
+      .storage()
+      .bucket(storageBucket)
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimeType
+          }
+        }
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${storageBucket}/o/${imageFileName}?alt=media`;
+        console.log(imageUrl);
+        return res.json({ imageUrl });
+      })
+      .catch(err => {
+        console.error(err);
+        return res.status(500).json({ general: err.message });
+      });
+  });
+
+  busboy.end(req.rawBody);
+};
--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const authMiddleware = require('./util/authMiddleware');
-const {docs, signup, login, getAllSections, getPosts, getPost, addPost, uploadUserImage} = require('./handlers');
+const {
+  docs,
+  signup,
+  login,
+  getAllSections,
+  getPosts,
+  getPost,
+  addPost,
+  uploadUserImage,
+  uploadPostTitleImage
+} = require('./handlers');
 
 // cors
 app.use(cors());
@@ -18,9 +28,10 @@ app.post('/signup', signup);
 app.post('/login', login);
 app.get('/sections', authMiddleware, getAllSections);
 app.get('/:section/posts', authMiddleware, getPosts);
-app.get('/posts/:id', authMiddleware, getPost)
-app.post('/:section/posts', authMiddleware, addPost)
+app.get('/posts/:id', authMiddleware, getPost);
+app.post('/:section/posts', authMiddleware, addPost);
 app.post('/user/image', authMiddleware, uploadUserImage);
+app.post('/post/image', authMiddleware, uploadPostTitleImage);
 
 // ============================================================
 // Create and Deploy Your First Cloud Functions
